ab01dad555e74037e9eea3d22aa13c4e1c84f542
sunhy427 <396910577@qq.com>
2025-06-05 06:44:07 +0800

    add res 302 location

--- src/api/config.js
+++ src/api/config.js
@@ -47,10 +47,12 @@ service.interceptors.request.use(
 //响应拦截
 service.interceptors.response.use(
   (response) => {
+    if (response.status === 302 && response.headers.location) {
+      window.location.href = response.headers.location // 跳转到重定向地址
+      return new Promise(() => {}) // 中断当前请求链，避免后续处理
+    }
+    
     let res = response.data
-    // if (res.status === 1) {
-    //   return Promise.resolve(res);
-    // }
     if (res.status === 0) {
       if (res.message) {
         ElMessage.error(res.message)
